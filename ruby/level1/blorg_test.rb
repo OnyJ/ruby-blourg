96b3264da23998da4cb9d30da2c6e3e259b13a17
Josselin <julien.josselin75@hotmail.fr>
2022-07-10 13:09:58 +0200

    :test_tube: add get_french_character test
    Failing test, Blorg class, testing 1 character translation

--- ruby/level1/blorg_test.rb
+++ ruby/level1/blorg_test.rb
@@ -4,3 +4,16 @@
 # Veillez à tester les situations et erreur inattendues
 
 require_relative 'blorg'
+
+describe Blorg do
+  context 'character translation' do
+    describe '#get_french_character' do
+      it 'returns the French character matching the given Blourg character' do
+        expect(Blorg.get_french_character('rlgo')).to eq 'G'
+        expect(Blorg.get_french_character('glhu')).to eq 'N'
+        expect(Blorg.get_french_character('hrbo')).to eq 'U'
+        expect(Blorg.get_french_character('rblg')).to eq '!'
+      end
+    end
+  end
+end
