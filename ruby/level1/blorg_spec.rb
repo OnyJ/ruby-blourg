e368937f652df3d89f64110ae8d20bb14060d5d2
Josselin <julien.josselin75@hotmail.fr>
2022-07-10 15:48:47 +0200

    :test_tube: add tests for character is_translatable

--- ruby/level1/blorg_spec.rb
+++ ruby/level1/blorg_spec.rb
@@ -15,5 +15,35 @@ describe Blorg do
         expect(Blorg.get_french_character('rblg')).to eq '!'
       end
     end
+
+    describe '#is_translatable?' do
+      context 'french character matching' do
+        it 'returns true if the TRANSLATION_TABLE contains the key' do
+          expect(Blorg.is_translatable?('J')).to be true
+          expect(Blorg.is_translatable?('Z')).to be true
+          expect(Blorg.is_translatable?('E')).to be true
+        end
+        it "returns false if the TRANSLATION_TABLE doesn't contain the key" do
+          expect(Blorg.is_translatable?(')')).to be false
+          expect(Blorg.is_translatable?('#')).to be false
+          expect(Blorg.is_translatable?('3')).to be false
+          expect(Blorg.is_translatable?(' ')).to be false
+        end
+      end
+      context 'blourg character matching' do
+        it 'returns true if the TRANSLATION_TABLE contains the value' do
+          expect(Blorg.is_translatable?('olgr')).to be true
+          expect(Blorg.is_translatable?('rhol')).to be true
+          expect(Blorg.is_translatable?('uhog')).to be true
+        end
+        it "returns false if the TRANSLATION_TABLE doesn't contain the value" do
+          expect(Blorg.is_translatable?(' ')).to be false
+          expect(Blorg.is_translatable?('xxxx')).to be false
+          expect(Blorg.is_translatable?('po')).to be false
+          expect(Blorg.is_translatable?('12-c')).to be false
+          expect(Blorg.is_translatable?('1234')).to be false
+        end
+      end
+    end
   end
 end
