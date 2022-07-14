ed9f8595a2cf91f7372a6971f54a5d674e3a9c17
Josselin <julien.josselin75@hotmail.fr>
2022-07-14 22:28:57 +0200

    :white_check_mark: change Blorg test expectation for BlorgValidation
    expect BlorgValidation module for all validation function, in blorg_spec.rb

--- ruby/level1/blorg_spec.rb
+++ ruby/level1/blorg_spec.rb
@@ -17,19 +17,6 @@ describe '#decode' do
   end
 end
 
-describe '#blourg_is_valid?' do
-  it 'returns true if Blourg string is valid' do
-    expect(Blorg.blourg_is_valid?('rgob rhol')).to be true
-    expect(Blorg.blourg_is_valid?('  rhol ')).to be true
-  end
-  it 'returns false if Blourg string is not valid' do
-    expect(Blorg.blourg_is_valid?('ok rgob')).to be false
-    expect(Blorg.blourg_is_valid?('ab cde')).to be false
-    expect(Blorg.blourg_is_valid?('a')).to be false
-    expect(Blorg.blourg_is_valid?('rgubrgub')).to be false
-  end
-end
-
 describe '#char_into_french' do
   it 'returns the French character matching the given Blourg character' do
     expect(BlorgChar.char_into_french('rlgo')).to eq 'G'
@@ -51,15 +38,15 @@ end
 describe '#translatable?' do
   context 'french character verification' do
     it 'returns true if the TRANSLATION_TABLE contains the key' do
-      expect(Blorg.translatable?('J')).to be true
-      expect(Blorg.translatable?('Z')).to be true
-      expect(Blorg.translatable?('E')).to be true
+      expect(BlorgValidation.translatable?('J')).to be true
+      expect(BlorgValidation.translatable?('Z')).to be true
+      expect(BlorgValidation.translatable?('E')).to be true
     end
     it "returns false if the TRANSLATION_TABLE doesn't contain the key" do
-      expect(Blorg.translatable?(')')).to be false
-      expect(Blorg.translatable?('#')).to be false
-      expect(Blorg.translatable?('3')).to be false
-      expect(Blorg.translatable?(' ')).to be false
+      expect(BlorgValidation.translatable?(')')).to be false
+      expect(BlorgValidation.translatable?('#')).to be false
+      expect(BlorgValidation.translatable?('3')).to be false
+      expect(BlorgValidation.translatable?(' ')).to be false
     end
   end
 end
@@ -67,16 +54,29 @@ end
 describe '#translatable?' do
   context 'blourg character verification' do
     it 'returns true if the TRANSLATION_TABLE contains the value' do
-      expect(Blorg.translatable?('olgr')).to be true
-      expect(Blorg.translatable?('rhol')).to be true
-      expect(Blorg.translatable?('uhog')).to be true
+      expect(BlorgValidation.translatable?('olgr')).to be true
+      expect(BlorgValidation.translatable?('rhol')).to be true
+      expect(BlorgValidation.translatable?('uhog')).to be true
     end
     it "returns false if the TRANSLATION_TABLE doesn't contain the value" do
-      expect(Blorg.translatable?(' ')).to be false
-      expect(Blorg.translatable?('xxxx')).to be false
-      expect(Blorg.translatable?('po')).to be false
-      expect(Blorg.translatable?('12-c')).to be false
-      expect(Blorg.translatable?('1234')).to be false
+      expect(BlorgValidation.translatable?(' ')).to be false
+      expect(BlorgValidation.translatable?('xxxx')).to be false
+      expect(BlorgValidation.translatable?('po')).to be false
+      expect(BlorgValidation.translatable?('12-c')).to be false
+      expect(BlorgValidation.translatable?('1234')).to be false
     end
+  end
+end
+
+describe '#blourg_is_valid?' do
+  it 'returns true if Blourg string is valid' do
+    expect(BlorgValidation.blourg_is_valid?('rgob rhol')).to be true
+    expect(BlorgValidation.blourg_is_valid?('  rhol ')).to be true
+  end
+  it 'returns false if Blourg string is not valid' do
+    expect(BlorgValidation.blourg_is_valid?('ok rgob')).to be false
+    expect(BlorgValidation.blourg_is_valid?('ab cde')).to be false
+    expect(BlorgValidation.blourg_is_valid?('a')).to be false
+    expect(BlorgValidation.blourg_is_valid?('rgubrgub')).to be false
   end
 end
